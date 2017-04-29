699391d4842591518b787cf9ec7804701621e3c5
VVhug <1561004189@qq.com>
2017-04-29 19:14:20 +0800

    UDP

---
+++ Test/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Net;
+
+namespace Test
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            textBox2.Text = "";
+
+            try
+            {
+
+                IPHostEntry hostInfo = Dns.GetHostEntry(textBox1.Text.Trim());
+
+                textBox2.Text = "主机名：" + Dns.GetHostName() + "\r\n";
+
+                foreach (IPAddress ipadd in hostInfo.AddressList)
+                {
+
+                    textBox2.Text += ipadd.ToString() + "\r\n";
+
+                }
+
+            }
+
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message.ToString());
+
+            }
+
+        }
+
+
+    }
+}
---
+++ UDP/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+
+namespace Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            UdpClient client = new UdpClient(new IPEndPoint(IPAddress.Any, 0));
+            IPEndPoint endpoint = new IPEndPoint(IPAddress.Parse("255.255.255.255"), 7788);
+            byte[] buf = Encoding.Default.GetBytes("Hello from UDP broadcast");
+            Thread t = new Thread(new ThreadStart(RecvThread));
+            t.IsBackground = true;
+            t.Start();
+            while (true)
+            {
+                client.Send(buf, buf.Length, endpoint);
+                Thread.Sleep(1000);
+            }
+        }
+
+        static void RecvThread()
+        {
+            UdpClient client = new UdpClient(new IPEndPoint(IPAddress.Any, 7788));
+            IPEndPoint endpoint = new IPEndPoint(IPAddress.Any, 0);
+            while (true)
+            {
+                byte[] buf = client.Receive(ref endpoint);
+                string msg = Encoding.Default.GetString(buf);
+                Console.WriteLine(msg);
+            }
+        }
+    }
+}
+
+
